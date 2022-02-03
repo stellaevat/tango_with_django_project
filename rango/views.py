f0beb153453ac931b96c091a08b7bdb4a8e95b78
Stella Eva Tsiapali <2526934t@student.gla.ac.uk>
2022-02-03 14:02:09 +0000

    (7.2) end of chapter 7, tests passed

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -40,10 +41,36 @@ def add_category(request):
         form = CategoryForm(request.POST)
     
         if form.is_valid():
-            form.save(commit=True)
+            cat = form.save(commit=True)
             return redirect('/rango/')
         else:
             print(form.errors)
         
     return render(request, 'rango/add_category.html', {'form':form})
     
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+        
+    if category is None:
+        return redirect('/rango/')
+        
+    form = PageForm()
+    
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.category = category
+            page.views = 0
+            page.save()
+            return redirect(reverse('rango:show_category',
+                                    kwargs={'category_name_slug':category_name_slug}))
+        else:
+            print(form.errors)
+    
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
